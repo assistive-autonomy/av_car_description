308c9459a1ffa4c8114f0c6633cd4f7df942c3c9
Alejandro Bordallo <alex.bordallo@ed.ac.uk>
2026-03-11 20:12:36 +0000

    Migrate to Zenoh and cleanup (#44)

--- dev.sh
+++ dev.sh
@@ -3,21 +3,15 @@
 # Build docker dev stage and add local code for live development
 # ----------------------------------------------------------------
 
-CYCLONE_VOL=""
 BASH_CMD=""
-
-# Default cyclone_dds.xml path
-CYCLONE_DIR=/home/$USER/cyclone_dds.xml
 
 # Function to print usage
 usage() {
     echo "
-Usage: dev.sh [-b|bash] [-l|--local] [-h|--help]
+Usage: dev.sh [-b|bash] [-h|--help]
 
 Where:
     -b | bash       Open bash in docker container (Default in dev.sh)
-    -l | --local    Use default local cyclone_dds.xml config
-    -l | --local    Optionally point to absolute -l /path/to/cyclone_dds.xml
     -h | --help     Show this help message
     "
     exit 1
@@ -29,16 +23,6 @@ while [[ "$#" -gt 0 ]]; do
         -b|bash)
             BASH_CMD=bash
             ;;
-        -l|--local)
-            # Avoid getting confused if bash is written where path should be
-            if [ -n "$2" ]; then
-                if [ ! $2 = "bash" ]; then
-                    CYCLONE_DIR="$2"
-                    shift
-                fi
-            fi
-            CYCLONE_VOL="-v $CYCLONE_DIR:/opt/ros_ws/cyclone_dds.xml"
-            ;;
         -h|--help)
             usage
             ;;
@@ -49,14 +33,6 @@ while [[ "$#" -gt 0 ]]; do
     esac
     shift
 done
-
-# Verify CYCLONE_DIR exists
-if [ -n "$CYCLONE_VOL" ]; then
-    if [ ! -f "$CYCLONE_DIR" ]; then
-        echo "$CYCLONE_DIR does not exist! Please provide a valid path to cyclone_dds.xml"
-        exit 1
-    fi
-fi
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
@@ -70,5 +46,4 @@ docker run -it --rm --net host \
     -v /etc/localtime:/etc/localtime:ro \
     -v ./av_car_description:/opt/ros_ws/src/car_description \
     -v ./av_car_meshes:/opt/ros_ws/src/av_car_meshes \
-    $CYCLONE_VOL \
     av_car_description:latest-dev
